f1864d082f1a38e4ed12f8379be6309b256b7793
Hari Prasad <hari.kvana@gmail.com>
2016-07-28 16:28:06 +0530

    show call added

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -11,7 +11,11 @@ class PostController < ApplicationController
 
   def show
   	   raw_response = RestClient.get "http://jsonplaceholder.typicode.com/posts/#{params[:id]}"
-       render json: {response: JSON.parse(raw_response)}, status: :ok
+  	   case raw_response.code
+  	   when 200
+       	render json: {response: JSON.parse(raw_response)}, status: :ok
+       else
+  		render json: {response: "Unknown Request"}, status: :un_processible_entity
   end
 
 end

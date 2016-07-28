cceed723ec49cb39e73bb00631a95fc03b56e4e2
Hari Prasad <hari.kvana@gmail.com>
2016-07-28 16:13:05 +0530

    first commit

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,11 @@
+class PostController < ApplicationController
+  def index
+  	raw_response = RestClient.get 'http://jsonplaceholder.typicode.com/posts'
+	case raw_response.code 
+  	when 200
+  		render json: {response: JSON.parse(raw_response.body)}, statu: :ok
+  	else
+  		render json: {response: "Unknown Request"}, status: :un_processible_entity
+  	end
+  end
+end
